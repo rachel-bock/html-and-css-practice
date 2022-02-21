acffec63bc36fb06f42ae5da154e1df07325c569
Rachel Bock <rachel.online@gmail.com>
2022-02-21 12:07:14 -0700

    Add check for winning combinations in rows

--- script.js
+++ script.js
@@ -6,6 +6,57 @@ let currPlayer = "X";
 let gameOver = false;
 let board = new Array(10).fill(null);
 
+function checkWinner(){
+    // if there is a winning combination on the board, the game is over.
+    // If the game is over, update the header on who won.
+
+    let winner = currPlayer;
+
+    // Check rows for winning combination. 
+    // Check row 1 for winning combination.   
+    if (board[1]== board[2] && board[2] == board[3] && board[3] != ""){
+        header.innerText = `${winner} won the game!`
+        for(let i = 1; i < 4; i++){
+            tile = document.getElementById("tile"+i);
+            tile.classList.add("winner");
+        }
+        gameOver = true;
+        return;
+    }
+
+    // Check row 2 for winning combination.
+    if (board[4]== board[5] && board[5] == board[6] && board[6] != ""){
+        header.innerText = `${winner} won the game!`
+        for(let i = 4; i <= 6; i++){
+            tile = document.getElementById("tile"+i);
+            tile.classList.add("winner");
+        }
+        gameOver = true;
+        return;
+    }
+
+    // check row 3 for winning combination.
+    if (board[7]== board[8] && board[8] == board[9] && board[9] != ""){
+        header.innerText = `${winner} won the game!`
+        for(let i = 7; i <= 9; i++){
+            tile = document.getElementById("tile"+i);
+            tile.classList.add("winner");
+        }
+        gameOver = true;
+        return;
+    }
+
+    // If there is no winner yet, change current player to the opposite player.
+    if (currPlayer == playerX){
+        currPlayer = playerO;
+    } else {
+        currPlayer = playerX;
+    }
+    // And update the header text.
+    header.innerText = `${currPlayer}'s turn!`
+    
+}
+
 function setTile(e){
 
     // Don't allow changing tiles if the game has completed.
@@ -13,14 +64,26 @@ function setTile(e){
         return;
     }
 
+    // If the tile already has a value, then don't allow changes.
+    if(this.innerText != ""){
+        return;
+    }
 
-    let tile = e.id;
+    // Capture the tile id to be updated.
+    let tile = this.id;
+    tileNum = tile[4];
+        
+    // For testing purposes.
     console.log("Tile is ", tile);
+    console.log("TileNum is ", tileNum);
     
     // Mark the board on the screen and in the board tracker.
     this.innerText = currPlayer;
-    board[tile] = currPlayer;
-    
+    board[tileNum] = currPlayer;
+
+    // Check for a winner.
+    checkWinner();
+
 }
 
 
@@ -52,7 +115,7 @@ function setUpGame(){
         tile.classList.add("tile");
         tile.id = tileNum;
         tile.innerText = "";
-        tile.addEventListener("click", (e) => setTile(e));
+        tile.addEventListener("click", setTile);
         gameBoard.appendChild(tile);
     }
 }
